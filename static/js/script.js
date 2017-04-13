7977e4766840f023bdfabf85536f680784e70a68
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2017-04-13 15:18:05 -0300

    arrumado a estrutura

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
-function getNome(){
+function telaInicial(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET','view/login.html');
+	xhr.onload = function(){
+		if(xhr.readyState === 4){
+			document.getElementById('conteudo').innerHTML = xhr.responseText;
+		}
+	}
+	xhr.send(null);
+}
+
+function realizarLogin(){
     var name = $('#name').val();
-
-
+	
 }
